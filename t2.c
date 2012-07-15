1504cc751ee9c4529d929d2720ea90e4e74552bb
KDM <kdm@docstream.no>
2012-07-15 20:27:03 +0200

    more apr testing

--- t2.c
+++ t2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-#include <apr_general.h>
-#include <apr_getopt.h>
+#include <apr-1.0/apr_general.h>
+#include <apr-1.0/apr_getopt.h>
 
 int
 main(int argc, const char * args[])
 {
     apr_initialize();
     printf("Do nada...%s ", "æøå");
+    apr_pool_t * pool;
+
+
     apr_terminate();
+
     return 0;
 }
 
